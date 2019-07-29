ca372335586556c6449decc9fa9ab51896c91e74
Rodrigo Palmeira <rpalmeira1999@gmail.com>
2019-07-29 18:08:24 -0300

    Primeiro Commit

---
+++ app/Lesson.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    //
+    /*
+    public function users(){
+        return $this->hasMany('App\User','lesson_user','lesson_id','DRE');
+    }
+    */
+}
---
+++ app/User.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'DRE','name', 'email', 'password','age'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    protected $primarykey = "DRE";
+
+    public function lessons(){
+        return $this->hasMany('App\Lesson','lesson_user','DRE','lesson_id');
+    }
+
+
+}
